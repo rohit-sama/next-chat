eb64b781bf215a062be868bd1ad9ad83f67625a4
rohit-sama <rohit9804singh@gmail.com>
2023-07-29 16:26:20 +0530

    database connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
 import Button from '@/components/ui/Button'
-import Image from 'next/image'
+import {db} from '@/lib/db'
 
-export default function Home() {
+export default async function Home() {
   return (
-    <>
-    <div className='text-red-500'>page</div>
-    <Button >
-      dgdfg
-    </Button>
-    </>
+  
   )
 }
