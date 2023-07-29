9a0e2cd88f7d57e7fe7515062ac51f0599eb701b
rohit-sama <rohit9804singh@gmail.com>
2023-07-29 17:18:55 +0530

    auth backend done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@ import {db} from '@/lib/db'
 
 export default async function Home() {
   return (
-  
+  <div></div>
   )
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,60 @@
+import { NextAuthOptions } from "next-auth";
+import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
+import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+
+function getEnv() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!clientId || !clientSecret) {
+    throw new Error(`Environment variable ${name} is missing`);
+  }
+  return { clientId, clientSecret };
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getEnv().clientId,
+      clientSecret: getEnv().clientSecret,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = ((await db.get(`user:${user.id}`)) as User) || null;
+
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
+    },
+    async session({ session, token }) {
+        if (token) {
+            session.user.id = token.id;
+            session.user.name = token.name;
+            session.user.email = token.email;
+            session.user.image = token.picture;
+        }
+        return session;
+    
+    },
+    redirect() {
+    return '/dashboard'
+    }
+  },
+};
