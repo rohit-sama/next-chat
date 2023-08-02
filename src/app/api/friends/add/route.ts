22da457617892c0399a86bc67c63a35412c6b317
rohit-sama <rohit9804singh@gmail.com>
2023-08-02 23:24:45 +0530

    layout for dashboard

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -11,13 +11,16 @@ export async function POST(req: Request) {
 
     const { email: emailToAdd } = addFriendsValidator.parse(body.email);
 
-    const isToAdd = await fetchRedis("get", `user:email:${emailToAdd}`) as string;
+    const isToAdd = (await fetchRedis(
+      "get",
+      `user:email:${emailToAdd}`
+    )) as string;
 
     if (!isToAdd) {
       return new Response("User not found", { status: 404 });
     }
 
-    const session = await getServerSession(authOptions)
+    const session = await getServerSession(authOptions);
     console.log(session);
     if (!session) {
       return new Response("Unauthorized", { status: 401 });
@@ -27,24 +30,31 @@ export async function POST(req: Request) {
       return new Response("You can't add yourself", { status: 400 });
     }
 
-    const isAlreadyAdded = await fetchRedis('sismember', `user:${isToAdd}:incoming_friend_requests`, session.user.id) as 0 | 1
- if(isAlreadyAdded) {
-    return new Response("user is already added", { status: 400 })
- }
+    const isAlreadyAdded = (await fetchRedis(
+      "sismember",
+      `user:${isToAdd}:incoming_friend_requests`,
+      session.user.id
+    )) as 0 | 1;
+    if (isAlreadyAdded) {
+      return new Response("user is already added", { status: 400 });
+    }
 
- const isAlreadyFriend = await fetchRedis('sismember', `user:${session.user.id}:friends`, isToAdd) as 0|1
- if(isAlreadyFriend) {
-    return new Response("You are already friends", { status: 400 })
- }
+    const isAlreadyFriend = (await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      isToAdd
+    )) as 0 | 1;
+    if (isAlreadyFriend) {
+      return new Response("You are already friends", { status: 400 });
+    }
 
+    db.sadd(`user:${isToAdd}:incoming_friend_requests`, session.user.id);
 
- db.sadd(`user:${isToAdd}:incoming_friend_requests`, session.user.id);
-
- return new Response("Friend request sent", { status: 200 });
-} catch (error) {
+    return new Response("Friend request sent", { status: 200 });
+  } catch (error) {
     if (error instanceof z.ZodError) {
       return new Response("invalid request", { status: 422 });
     }
     return new Response("Internal server error", { status: 500 });
-}
+  }
 }
