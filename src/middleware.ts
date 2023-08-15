7fc0bb1297cdab6b8ea85da832db20c3ed4f8dd3
rohit-sama <rohit9804singh@gmail.com>
2023-08-15 17:29:50 +0530

    added middleware for routes

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { getToken } from 'next-auth/jwt'
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+
+export default withAuth(
+    async function middleware(req) {
+        const pathname = req.nextUrl.pathname   
+        const isauth = await getToken({req})
+        const isLogin = pathname.startsWith('/login')
+
+        const sensetive = ['/dashboard']
+        const isacces = sensetive.some((path) => pathname.startsWith(path))
+
+        if (isLogin) {
+            if (isauth) {
+                return NextResponse.redirect(new URL('/dashboard', req.url))
+            }
+            return NextResponse.next()
+            }
+            if (!isauth && isacces) {
+                return NextResponse.redirect(new URL('/login', req.url))
+            }
+            if (pathname === "/") {
+                return NextResponse.redirect(new URL('/dashboard', req.url))
+            }
+    },{
+        callbacks :{
+            async authorized(){
+                return true
+            },
+        }
+    }
+)
+
+export const comfig = {
+    matchers: ['/', '/login', '/dashboard/:path*']
+}
